84b8f348f7f65feae9f9370995e238a0711b7711
marilia-borgo <marilia.bm01@gmail.com>
2023-01-11 11:49:09 -0300

    exercicio seis (ultimo dos faceis)

---
+++ 06Reversestring.js
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/reverse-string/
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+
+let s = ["h","e","l","l","o"]
+
+string = s.reverse()
+console.log(string)
+//leetcode
+// return string
