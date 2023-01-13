69316163a3816730fb72a1ff0331c06c96dc084d
marilia-borgo <marilia.bm01@gmail.com>
2023-01-13 16:57:02 -0300

    testes por exercício 10

--- teste.py
+++ teste.py
@@ -1,7 +1,10 @@
 from . import countRotas
 
 def test_chega_inicio_fim():
-    assert countRotas.rotas_possiveis([2,3,6,8,4],1,3,5) == 1
+    assert countRotas.solution_1([2,3,6,8,4],1,3,5) == 4
 
-def quantidade_rotas_totais():
-    assert countRotas.rotas_possiveis([2,3,6,8,4],1,3,5) == 4
+def test_segundo_caso_leetcode():
+    assert countRotas.solution_1([4,3,1],1,0,6) == 5
+
+def test_nenhuma_rota_e_possivel():
+    assert countRotas.solution_1([5,2,1],0,2,3) == 0
