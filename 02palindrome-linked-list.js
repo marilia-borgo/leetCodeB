29e12aef56121a606cc456d5b21b6c6c7015b61a
marilia-borgo <marilia.bm01@gmail.com>
2023-01-26 13:53:49 -0300

    02 no vscode

--- 02palindrome-linked-list.js
+++ 02palindrome-linked-list.js
@@ -4,42 +4,54 @@
 // Input: head = [1,2]
 // Output: false
 
+//leetcode
+// var isPalindrome = function(head) {
+//     let slow = head;
+//     let fast = head;
+//     while (fast && fast.next) {
+//       slow = slow.next;
+//       fast = fast.next.next;
+//     }
+//     let prev = null;
+//     let curr = slow;
+//     while (curr) {
+//       let next = curr.next;
+//       curr.next = prev;
+//       prev = curr;
+//       curr = next;
+//     }
+//     let p1 = head;  
+//     let p2 = prev;
+//     while (p2) {
+//       if (p1.val !== p2.val) return false;
+//       p1 = p1.next;
+//       p2 = p2.next;
+//     }
+//     return true;
+//   }
 
-//Essa era a minh solução original, mas o leet code inicia como
-//linked list não como array, logo deu errado.
-
-
-// let head = [1,2]
-// let listaRevertida = head.map((x) => x); 
-// let resposta = listaRevertida.reverse()
-// if (resposta.every((value, index) => value === head[index])){
-//     return true
-// }
-// return false
-
-
-//solução pro linked list que ta no leet code
-var isPalindrome = function(head) {
-    let slow = head;
-    let fast = head;
-    while (fast && fast.next) {
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-    let prev = null;
-    let curr = slow;
-    while (curr) {
-      let next = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = next;
-    }
-    let p1 = head;  
-    let p2 = prev;
-    while (p2) {
-      if (p1.val !== p2.val) return false;
-      p1 = p1.next;
-      p2 = p2.next;
-    }
-    return true;
+//solução para rodar no vs code
+  head = [1,2,2,1]
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
+  let prev = null;
+  let curr = slow;
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  let p1 = head;  
+  let p2 = prev;
+  while (p2) {
+    if (p1.val !== p2.val) 
+    console.log('false')
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+  console.log('true');
